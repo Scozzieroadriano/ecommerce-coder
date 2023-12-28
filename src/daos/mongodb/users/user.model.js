15d695547a308b60c25b1f6342d9382c5bfcc01d
Scozzieroadriano <scozzieroadriano@gmail.com>
2023-12-28 00:34:43 -0300

    passport

--- src/daos/mongodb/users/user.model.js
+++ src/daos/mongodb/users/user.model.js
@@ -3,11 +3,11 @@ import { Schema, model } from "mongoose";
 export const usersCollectionName = "users";
 
 const userSchema = new Schema({
-    first_name: { type: String, required: true },
+    first_name: { type: String, required: false },
     last_name: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    age: { type: Number, required: true },
+    age: { type: Number, required: true, default: 0 },
     cart: { type: Schema.Types.ObjectId, ref: 'carts' },
     role: { type: String, default: 'user' },
 });
