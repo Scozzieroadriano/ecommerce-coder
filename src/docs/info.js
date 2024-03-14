127166bca04b36bb2fabd4016635fe0a6a123497
Scozzieroadriano <scozzieroadriano@gmail.com>
2024-03-13 22:29:53 -0300

    docs para put y getbyid con token

--- src/docs/info.js
+++ src/docs/info.js
@@ -4,7 +4,7 @@ export const info = {
         info: {
             title: "Ecommerce API - CoderHouse",
             version: "1.0.0",
-            description: "Backend para e-commerce desarrollado junto con coderhouse, cuenta con login,carrito de compras, crud de productos, orden de compra, entre otras cosas.",
+            description: "Backend para e-commerce desarrollado junto con coderhouse, cuenta con login, carrito de compras, crud de productos, orden de compra, entre otras cosas.",
         },
         servers:[
             {
@@ -14,6 +14,21 @@ export const info = {
             {
                 url: "https://ecommerce-api.herokuapp.com",
                 description: "Production server"
+            }
+        ],
+        components: {
+            securitySchemes: {
+                BearerAuth: {
+                    type: "apiKey",
+                    in: "header",
+                    name: "Authorization",
+                    description: "Utiliza este campo para proporcionar un token JWT de autorización."
+                }
+            }
+        },
+        security: [
+            {
+                BearerAuth: []
             }
         ]
     },
